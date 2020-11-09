2d4285ac4db22ba9ef3f3d1866fca3761d948697
carolinmaisenbacher <carolin.maisenbacher@code.berlin>
2020-11-09 09:30:51 +0000

    Move Overview view into seperate file
    And add missing babel dependency to make husky work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,17 @@
 // DOCUMENTATION: http://styletron.org
 import React from 'react';
+import PropTypes from 'prop-types';
 import { withApollo } from 'lib/apollo';
 import requireAuthentification from 'lib/requireAuthentification';
 
 import { Dashboard } from 'layouts';
-import { LogoutButton } from 'atoms/buttons';
+import { Overview } from 'views/Overview';
 
 const IndexPage = ({ user }) => {
   // an alternative hook based API
   return (
     <Dashboard>
-      <h1>{`Welcome ${user.username}!`}</h1>
-      <h3>You successfully logged in.</h3>
-      <LogoutButton />
+      <Overview user={user} />
     </Dashboard>
   );
 };
@@ -20,6 +19,10 @@ const IndexPage = ({ user }) => {
 IndexPage.getInitialProps = async (context) => {
   // redirects if not logged in, and adds user object to props
   return await requireAuthentification(context);
+};
+
+IndexPage.propTypes = {
+  user: PropTypes.shape().isRequired
 };
 
 export default withApollo(IndexPage);
